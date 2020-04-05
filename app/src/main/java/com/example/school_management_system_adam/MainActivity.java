ace5c42fbc8a4b834d2bc183baa4b21e484ee779
harshit-soora <soora.iitkgp@gmail.com>
2020-04-05 22:35:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/school_management_system_adam/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.school_management_system_adam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
