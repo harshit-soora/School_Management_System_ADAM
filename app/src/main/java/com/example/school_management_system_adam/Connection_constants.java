83e51887d04503daff65cac1582d735ce28e6d2d
harshit-soora <soora.iitkgp@gmail.com>
2020-04-12 10:35:12 +0530

    Added the background images

---
+++ app/src/main/java/com/example/school_management_system_adam/Connection_constants.java
@@ -0,0 +1,12 @@
+package com.example.school_management_system_adam;
+
+public class Connection_constants {
+
+    //For me localhost is 172.17.0.1 for Xampp server
+    //Make sure you change the url appropriately
+    //Place the Android_ADAM folder inside htdocs for Xampp server
+
+    private static final String ROOT_URL = "http://172.17.0.1/Android_ADAM/v1/registerUser.php";
+
+    public static final String REGISTER_URL = "http://172.17.0.1/Android_ADAM/v1/userLogin.php";
+}
