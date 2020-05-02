d0e18fe279b0b3b4b23b3a5c3caa20fe419a3888
harshit-soora <soora.iitkgp@gmail.com>
2020-05-02 22:31:02 +0530

    Database updated

--- Database/school.sql
+++ Database/school.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 20, 2020 at 01:39 PM
+-- Generation Time: May 02, 2020 at 06:59 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -51,7 +51,9 @@ CREATE TABLE `class` (
 --
 
 INSERT INTO `class` (`CID`, `CNAME`, `CSEC`) VALUES
-(28, 'II', 'A');
+(32, 'XII', 'B'),
+(33, 'II', 'C'),
+(35, 'VIII', 'A');
 
 -- --------------------------------------------------------
 
@@ -105,15 +107,21 @@ INSERT INTO `staff` (`TID`, `TNAME`, `TPASS`, `QUAL`, `SAL`) VALUES
 
 CREATE TABLE `subject` (
   `SID` int(11) NOT NULL,
-  `SNAME` varchar(150) NOT NULL
+  `SNAME` varchar(150) NOT NULL,
+  `STEACHER` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `subject`
 --
 
-INSERT INTO `subject` (`SID`, `SNAME`) VALUES
-(14, 'Science');
+INSERT INTO `subject` (`SID`, `SNAME`, `STEACHER`) VALUES
+(17, 'Economics', 'RamDas'),
+(18, 'Biology', 'Pankaj Kumar'),
+(19, 'EVS', 'Prakash Das'),
+(20, 'Chemistry', 'Narendra Kok'),
+(21, 'Chemistry', 'Kok'),
+(22, 'DBMS', 'Shamik');
 
 -- --------------------------------------------------------
 
@@ -137,7 +145,11 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
 (15, 'sanju', 'soora.iitkgp@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b'),
 (16, 'sanju123', 'soora123@gmail.com', '202cb962ac59075b964b07152d234b70'),
 (17, 'ramdutt', 'soora321@gmail.com', '116fb69d6772952c80ad5f02f038393f'),
-(18, 'iitkgp', 'iitkgp@ac.in', '9dc294ff36622cb3f41567890aa6d7f6');
+(18, 'iitkgp', 'iitkgp@ac.in', '9dc294ff36622cb3f41567890aa6d7f6'),
+(19, 'anshul', 'anshul@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4'),
+(20, 'Choudhary', 'chou@gmail.com', 'eb89f40da6a539dd1b1776e522459763'),
+(21, 'admin', 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3'),
+(22, 'rakesh', 'rakesh@gmail.com', '116fb69d6772952c80ad5f02f038393f');
 
 --
 -- Indexes for dumped tables
@@ -193,7 +205,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `class`
 --
 ALTER TABLE `class`
-  MODIFY `CID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `CID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `exam`
@@ -211,13 +223,13 @@ ALTER TABLE `staff`
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `SID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `SID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
